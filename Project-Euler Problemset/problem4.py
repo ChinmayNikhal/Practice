176aca150f681576023b8743c307f21f4c81c5eb
Chinmay Nikhal <nikhalchinmay@gmail.com>
2024-06-10 12:14:43 +0530

    Problem 4 Solved

--- Project-Euler Problemset/problem4.py
+++ Project-Euler Problemset/problem4.py
@@ -4,3 +4,18 @@ A palindromic number reads the same both ways. The largest palindrome made from 
 Find the largest palindrome made from the product of two 3-digit numbers.
 '''
 
+biggest = 0
+counter_a = 999
+
+while(counter_a >= 100):
+    counter_b = 999
+    while(counter_b >= 100):
+        temp = counter_a * counter_b
+        if(str(temp) == (str(temp))[::-1]):
+            if(biggest < temp):
+                biggest = temp
+
+        counter_b -= 1
+    counter_a -= 1
+
+print(biggest)
